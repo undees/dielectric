509a715e2bc6e31fe757417296c13fc9a880f354
Ian Dees <undees@gmail.com>
2009-05-31 13:42:03 -0700

    Added another station

--- dielectric.rb
+++ dielectric.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'hpricot'
 require 'tzinfo'
 require 'open-uri'
 require 'cgi'
@@ -9,22 +10,62 @@ get '/' do
 end
 
 get '/:station/:time' do |station, time|
-  halt 404 unless station.downcase == 'knrk'
+  station.downcase!
+  halt 404 unless ['knrk', 'kink'].include? station
 
   utc = Time.parse(time)
-  tz = TZInfo::Timezone.get('America/Los_Angeles')
-  local = tz.utc_to_local(utc)
-  clock = local.strftime('%H%M')
 
-  # This is just an example for teaching purposes.  Please be mindful
+  # Both stations are in Pacific time for now.
+  tz = TZInfo::Timezone.get('America/Los_Angeles')
+  off = tz.utc_to_local(utc)
+
+  # Oops, TZInfo leaves the time zone in UTC.
+  local = Time.local off.year, off.month, off.day, off.hour, off.min
+
+  # These are just examples for teaching purposes.  Please be mindful
   # of people's servers.
-  url = "http://mobile.yes.com/song.jsp?city=24&station=knrk_94.7&hm=#{clock}&a=0"
+
+  url_makers = {
+    'knrk' => lambda {|t| "http://mobile.yes.com/song.jsp?city=24&station=knrk_94.7&hm=#{local.strftime('%H%M')}&a=0"},
+    'kink' => lambda {|t| "http://playlist.kink.fm/today_lrp.asp?date=#{local.strftime('%m%d%Y')}"}
+  }
+
+  knrk_finder = lambda do |h, t|
+    no_match = [nil, nil]
+    title = (%r(<td>([^<]+)<br/>).match(h) || no_match)[1]
+    artist = (%r(<br/>by ([^\\<]+)<br/>).match(h) || no_match)[1]
+    [title, artist]
+  end
+
+  kink_finder = lambda do |h, t|
+    doc = Hpricot(h)
+    songs = (doc / 'tr.body').map do |row|
+      time_s, title, artist = (row / 'td').map do |col|
+        col.to_plain_text.strip
+      end
+      stamp = Time.parse(time_s)
+      time = Time.local(t.year, t.month, t.day, stamp.hour, stamp.min)
+      {:time => time, :artist => artist, :title => title}
+    end
+
+    result = songs.reverse.find {|r| r[:time] <= t}
+    result ? [result[:title], result[:artist]] : [nil, nil]
+  end
+
+  finders = {
+    'knrk' => knrk_finder,
+    'kink' => kink_finder
+  }
+
+  url_maker, finder = url_makers[station], finders[station]
+  halt 404 unless url_maker && finder
+
+  url = url_maker[local]
+  halt 404 unless url_maker
+
   headers = {'User-Agent' => 'Mozilla/5.0'}
   html = open(url, headers) {|f| f.read}
-
-  no_match = [nil, nil]
-  title = (%r(<td>([^<]+)<br/>).match(html) || no_match)[1]
-  artist = (%r(<br/>by ([^\\<]+)<br/>).match(html) || no_match)[1]
+  title, artist = finder[html, local]
 
   halt 404 unless title && artist
 
@@ -47,6 +88,7 @@ get '/stations' do
 <plist version="1.0">
 <array>
 	<string>KNRK</string>
+	<string>KINK</string>
 </array>
 </plist>
 HERE
