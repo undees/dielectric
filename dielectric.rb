3bd5796372746589ea5ee941f37341733a2336d4
Ian Dees <undees@gmail.com>
2009-06-08 09:34:41 -0700

    Small typo

--- dielectric.rb
+++ dielectric.rb
@@ -9,7 +9,7 @@ get '/' do
   redirect '/index.html'
 end
 
-get ':city/:station/:time' do |city, station, time|
+get '/:city/:station/:time' do |city, station, time|
   station.downcase!
   halt 404 unless ['knrk', 'kink'].include? station
 
