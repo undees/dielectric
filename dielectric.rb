a8e96a43a8ec679c7a3dab4fc7ede8a4296794bf
Ian Dees <undees@gmail.com>
2009-06-16 00:09:43 -0700

    Use approved API for songs

--- dielectric.rb
+++ dielectric.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'sinatra'
-require 'hpricot'
+require 'json'
 require 'tzinfo'
 require 'open-uri'
 require 'cgi'
@@ -10,63 +10,34 @@ get '/' do
 end
 
 get '/:city/:station/:time' do |city, station, time|
-  station.downcase!
-  halt 404 unless ['knrk', 'kink'].include? station
-
+  # All stations are in Pacific time for now.
   utc = Time.parse(time)
-
-  # Both stations are in Pacific time for now.
   tz = TZInfo::Timezone.get('America/Los_Angeles')
   off = tz.utc_to_local(utc)
 
   # Oops, TZInfo leaves the time zone in UTC.
   local = Time.local off.year, off.month, off.day, off.hour, off.min
+  day_of = Time.local off.year, off.month, off.day
+  days_ago = ((Time.now - day_of) / 86400.0).to_i
+  halt 404 unless (0..6).include? days_ago
 
-  # These are just examples for teaching purposes.  Please be mindful
-  # of people's servers.
+  url = "http://api.yes.com/1/log?name=#{station}&ago=#{days_ago}"
+  json = open(url) {|f| f.read}
+  songs = JSON.parse(json)['songs']
+  songs.map! {|s| s.merge({'at' => Time.parse(s['at'])})}
+  song = songs.reverse.first {|s| s.at < local}
 
-  url_makers = {
-    'knrk' => lambda {|t| "http://mobile.yes.com/song.jsp?city=24&station=knrk_94.7&hm=#{local.strftime('%H%M')}&a=0"},
-    'kink' => lambda {|t| "http://playlist.kink.fm/today_lrp.asp?date=#{local.strftime('%m%d%Y')}"}
-  }
-
-  knrk_finder = lambda do |h, t|
-    no_match = [nil, nil]
-    title = (%r(<td>([^<]+)<br/>).match(h) || no_match)[1]
-    artist = (%r(<br/>by ([^\\<]+)<br/>).match(h) || no_match)[1]
-    [title, artist]
+  unless song
+    url = "http://api.yes.com/1/recent?name=#{station}&max=100"
+    json = open(url) {|f| f.read}
+    songs = JSON.parse(json)['songs']
+    songs.map! {|s| s.merge({'at' => Time.parse(s['at'])})}
+    song = songs.first {|s| s.at < local}
   end
 
-  kink_finder = lambda do |h, t|
-    doc = Hpricot(h)
-    songs = (doc / 'tr.body').map do |row|
-      time_s, title, artist = (row / 'td').map do |col|
-        col.to_plain_text.strip
-      end
-      stamp = Time.parse(time_s)
-      time = Time.local(t.year, t.month, t.day, stamp.hour, stamp.min)
-      {:time => time, :artist => artist, :title => title}
-    end
+  halt 404 unless song
 
-    result = songs.reverse.find {|r| r[:time] <= t}
-    result ? [result[:title], result[:artist]] : [nil, nil]
-  end
-
-  finders = {
-    'knrk' => knrk_finder,
-    'kink' => kink_finder
-  }
-
-  url_maker, finder = url_makers[station], finders[station]
-  halt 404 unless url_maker && finder
-
-  url = url_maker[local]
-  halt 404 unless url_maker
-
-  headers = {'User-Agent' => 'Mozilla/5.0'}
-  html = open(url, headers) {|f| f.read}
-  title, artist = finder[html, local]
-
+  title, artist = song['title'], song['by']
   halt 404 unless title && artist
 
   response['Cache-Control'] = 'public; max-age=86400'
